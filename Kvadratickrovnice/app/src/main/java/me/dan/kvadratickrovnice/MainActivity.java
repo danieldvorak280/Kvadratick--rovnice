5b59edc74e743921d6cef0955b1517b0b32206f5
danieldvorak280 <dan.dvorak280@gmail.com>
2018-01-02 19:19:59 +0100

    Kvadratická rovnice

---
+++ Kvadratickrovnice/app/src/main/java/me/dan/kvadratickrovnice/MainActivity.java
@@ -0,0 +1,99 @@
+package me.dan.kvadratickrovnice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextA)
+    EditText aInput;
+
+    @BindView(R.id.editTextB)
+    EditText bInput;
+
+    @BindView(R.id.editTextC)
+    EditText cInput;
+
+    @BindView(R.id.textViewResult)
+    TextView resultView;
+
+    @BindView(R.id.vypisRovniceTextView)
+    TextView rovniceView;
+
+    private int a;
+    private int b;
+    private int c;
+    private double discriminant;
+    private double result1;
+    private double result2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick(View v) {
+        try {
+            a = Integer.parseInt(aInput.getText().toString());
+            b = Integer.parseInt(bInput.getText().toString());
+            c = Integer.parseInt(cInput.getText().toString());
+        } catch (NullPointerException ex) {
+            Toast.makeText(getApplicationContext(), "Zadejte číslo", Toast.LENGTH_SHORT).show();
+            return;
+        } catch (NumberFormatException ex) {
+            Toast.makeText(getApplicationContext(), "Zadejte číslo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        discriminant = (Math.pow(b, 2)) - (4 * a * c);
+        result1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        result2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+
+        if (a == 0 && b == 0 && c == 0) {
+            rovniceView.setText("0 = 0");
+        } else if (a == 0 && b == 0) {
+            rovniceView.setText(c + " = 0");
+        } else if (a == 0 && c == 0) {
+            rovniceView.setText(b + "x = 0");
+        } else if (b == 0 && c == 0) {
+            rovniceView.setText(a + "x² = 0");
+        } else if (a == 0) {
+            rovniceView.setText(b + "x + " + c + " = 0");
+        } else if (b == 0) {
+            rovniceView.setText(a + "x² + " + c + " = 0");
+        } else if (c == 0) {
+            rovniceView.setText(a + "x² + " + b + "x = 0");
+        } else {
+            rovniceView.setText(a + "x² + " + b + "x + " + c + " = 0");
+        }
+
+        if (a == 0 && b == 0 && c == 0) {
+            resultView.setText("Rovnice má nekonečně mnoho řešení");
+        } else if (a == 0 && b==0) {
+            resultView.setText("Rovnice nemá řešení");
+        } else if (discriminant > 0) {
+            resultView.setText("Rovnice má dvá kořeny:\n\nPrvní kořen je: " + result1 + "\nDruhý kořen je: " + result2);
+        } else if (discriminant == 0 && result1 != 0 && result2 != 0) {
+            resultView.setText("Rovnice má jedno řešení: " + result1);
+        } else if (discriminant < 0) {
+            resultView.setText("Rovnice nemá řešení v oboru reálných hodnot");
+        }
+        
+        aInput.setText("");
+        bInput.setText("");
+        cInput.setText("");
+    }
+}
